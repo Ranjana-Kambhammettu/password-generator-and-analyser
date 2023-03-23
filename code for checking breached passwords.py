9ff8519f933cae0f3fec5398817a697a72faae2a
Aryamaan20 <120029399+Aryamaan20@users.noreply.github.com>
2023-03-23 16:06:23 +0530

    Add files via upload
    python code that checks from the MySQL database if the password entered is a breached password or not

---
+++ code for checking breached passwords.py
@@ -0,0 +1,24 @@
+import mysql.connector
+
+# connect to the MySQL database
+connector= mysql.connector.connect(user='sanga123', 
+password='breached6789!!', host='breachedpasswords.mysql.database.azure.com',
+ port=3306, database='breachedpasswords')
+
+# get password from the user
+input = input("Enter password:")
+
+# define the query
+query = "SELECT * FROM passwords WHERE Password = %s"
+
+# execute the query with the user entered password as a parameter
+cursor = connector.cursor()
+cursor.execute(query, (input,))
+
+# check if any passwords were returned
+result = cursor.fetchone()
+if result:
+  print("Password entered is a breached password.")
+else:
+    print("Password entered is not a breached password")
+connector.close()
